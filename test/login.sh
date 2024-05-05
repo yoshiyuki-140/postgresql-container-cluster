45728305aefbd3d14dfcb3d4967459b7758a491d
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-05-05 22:08:53 +0900

    update : test dir

--- test/login.sh
+++ test/login.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # Login to the master database
-docker exec -it test-postgres-master-1 psql -U masteruser -d masterdb
+docker exec -it test-postgres-master-1 psql -U root -d masterdb
 
 # Login to the slave database
 # docker exec -it test-postgres-slave-1 psql -U slaveuser -d slavedb
